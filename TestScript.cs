586b19010e0a066f29c7d6ce7c210b15ce1aaed7
achintbansal <45305771+achintbansal@users.noreply.github.com>
2019-04-04 23:24:39 +0530

    Add files via upload
    test script

---
+++ TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject newObject;
+	NetworkPlayerScript playerScript;
+	GameObject networkPlayerObject;
+	void Start () {
+		 playerScript = newObject.GetComponent<NetworkPlayerScript> ();
+		networkPlayerObject = playerScript.playerUnit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
